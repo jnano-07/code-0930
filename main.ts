4b68ad5ef9aad983c3f90cd132588221c4f4a3eb
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-09-30 15:08:41 +0800

    TRY 1

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let 數 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    數 = randint(80, 100)
+    if (數 < 84) {
+        basic.showString("A-")
+    } else if (數 < 89) {
+        basic.showString("A")
+    } else {
+        basic.showString("A+")
+    }
+})
 basic.forever(function () {
 	
 })
