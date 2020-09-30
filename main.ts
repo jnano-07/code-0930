001e873266e5bf122afe042c3ebe4854d28353c0
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-09-30 15:23:46 +0800

    LAST

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 let 數 = 0
 input.onPinPressed(TouchPin.P0, function () {
+    basic.clearScreen()
     數 = randint(80, 100)
     basic.showString("" + (數))
-    if (數 < 84) {
+    if (數 <= 84) {
         basic.showString("A-")
-    } else if (數 < 89) {
+    } else if (數 <= 89) {
         basic.showString("A")
     } else if (數 >= 90) {
         basic.showString("A+")
